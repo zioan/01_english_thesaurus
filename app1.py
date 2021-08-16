3b562e6fea9d077f063cee733ec10af5c08a8a07
zioan <zaharia.ioan@gmail.com>
2021-08-16 16:08:04 +0200

    case_sensitive

--- app1.py
+++ app1.py
@@ -3,7 +3,11 @@ import json
 data = json.load(open("data.json"))
 
 def translate(w):
-  return data[w]
+  w = w.lower()
+  if w in data:
+    return data[w]
+  else:
+    return "The word doesn't exist. Please try again."
 
 word = input("Enter word:")
 
